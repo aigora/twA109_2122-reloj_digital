67cc50e7eaeefd8fdcbe2f2d939110b7ee635171
zeqilin <47862400+zeqilin@users.noreply.github.com>
2022-04-04 12:25:05 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,22 @@
+Serial* Arduino;
+char puerto[] = "COM5"; // Puerto serie en el que est¨¢ conectado Arduino
+char BufferEntrada[200];
+int bytesRecibidos;
+
+Arduino = new Serial((char*)puerto); // Crea conexi¨®n l¨®gica con Arduino
+if (Arduino->IsConnected())
+printf("Arduino conectado\n");
+else
+printf("Error\n");
+while (Arduino->IsConnected())
+{
+
+	bytesRecibidos = Arduino->ReadData(BufferEntrada, sizeof(char) * 199);
+	if (bytesRecibidos != -1) // Lectura de mensaje desde el puerto
+	{
+		BufferEntrada[bytesRecibidos - 1] = '\0';
+		printf(" %s\n", BufferEntrada);
+	}
+	else
+		Sleep(1000);
+}
