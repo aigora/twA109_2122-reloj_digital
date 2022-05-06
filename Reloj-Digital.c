ec7e63592d592b4affd84ea2e96d11261857b59b
lucia-phermosa <61051941+lucia-phermosa@users.noreply.github.com>
2022-05-06 11:06:32 +0200

    Update Reloj-Digital.c

--- Reloj-Digital.c
+++ Reloj-Digital.c
@@ -1,22 +1,49 @@
+#include "SerialPort.h" // Biblioteca para acceder al puerto serie
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <time.h>
+#include <string.h>
+
+#define TAM 30
 
 // Funciones prototipo
 int menu_ppal1(void);
 int menu_ppal2(void);
+char* Date_Time(void);
+char* formato_time(const struct tm*);
 
 int main(void)
 {
 	int opcion;
+	Serial* Arduino;
+	char puerto[] = "COM6"; // Puerto serie en el que esté conectado Arduino
+	char BufferEntrada[200];
+	int bytesRecibidos;
+	char fechayhora[TAM];
+	char* fyh;
+	int i, n;
+	
+	setlocale(LC_ALL, "es-ES");
 
-	if () // Si está conectado Arduino
+	Arduino = new Serial((char*)puerto); // Crea conexión lógica con Arduino
+
+	if (Arduino->IsConnected()) // Si está conectado Arduino
 	{
 		opcion = menu_ppal1();
 
 		switch (opcion)
 		{
-		case 1: break;
+		case 1: 
+				while (Arduino->IsConnected())
+			        {
+				    fyh = Date_Time();
+
+				    strcpy(fechayhora, fyh);
+
+				    Arduino->WriteData(fechayhora, strlen(fechayhora));
+			        }
+				break;
 		case 2: break;
 		case 3: break;
 		case 4: break;
@@ -34,6 +61,19 @@ int main(void)
 		case 4: break;
 		case 5: break;
 		}
+	}
+	
+	while (Arduino->IsConnected())
+	{
+
+		bytesRecibidos = Arduino->ReadData(BufferEntrada, sizeof(char) * 199);
+		if (bytesRecibidos != -1) // Lectura de mensaje desde el puerto
+		{
+			BufferEntrada[bytesRecibidos - 1] = '\0';
+			printf(" %s\n", BufferEntrada);
+		}
+		else
+			Sleep(1000);
 	}
 }
 
@@ -86,3 +126,30 @@ int menu_ppal2(void)
 	return opc;
 }
 
+// Función para obtener la fecha y hora del sistema
+char* Date_Time()
+{
+	
+	time_t t;
+	struct tm *tm;
+
+	t = time(NULL);
+	tm = localtime(&t);
+
+	return formato_time(tm);
+}
+
+// Función para hacer el formato con el que se va a imprimir la fecha y hora
+char* formato_time(const struct tm* timeptr)
+{
+	//static char result[TAM];
+	char* result = NULL;
+	result = (char*)malloc(sizeof(char) * TAM);
+	if (result == NULL)
+		printf("AVISO: Memoria insuficiente\n");
+	else
+	{
+		sprintf(result, "%.2d/%.2d/%d  %.2d:%.2d:%.2d\n", timeptr->tm_mday, 1 + timeptr->tm_mon, 1900 + timeptr->tm_year, timeptr->tm_hour, timeptr->tm_min, timeptr->tm_sec);
+	}
+	  return result;
+}
